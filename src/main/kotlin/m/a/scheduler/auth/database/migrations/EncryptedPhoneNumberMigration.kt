55caaf3e3c9a079f51cac944de75fe5755067e9a
mahmoud <mahmoud.afarideh@tapsi.cab>
2025-11-06 14:10:07 +0330

    Adds encrypted user phone number migrations

--- src/main/kotlin/m/a/scheduler/auth/database/migrations/EncryptedPhoneNumberMigration.kt
+++ src/main/kotlin/m/a/scheduler/auth/database/migrations/EncryptedPhoneNumberMigration.kt
@@ -13,21 +13,35 @@ class EncryptedPhoneNumberMigration(
     private val phoneNumberCrypto: PhoneNumberCrypto
 ) {
     @Changeset(order = 1, id = "encrypt_phone_number", author = "Mahmoud")
-    fun changeset(template: MongoTemplate) {
+    fun phoneOtpCryptoChangeSet(template: MongoTemplate) {
         template.findAll(Document::class.java, "phone_number_otp").forEach { phoneOtp ->
-            val phone = phoneOtp.get("phone", Document::class.java)
-            val phoneNumber = phone.get("phoneNumber", String::class.java)
-            val countryCode = phone.get("countryCode", String::class.java)
-            val encryptedPhoneNumber = phoneNumberCrypto.encrypt(
-                PhoneNumber(
-                    phoneNumber,
-                    PhoneNumber.CountryCode.entries.first { it.countryCode == countryCode }
-                )
-            )
-            phone["phoneNumber"] = encryptedPhoneNumber.phoneNumber
-            phone["countryCode"] = encryptedPhoneNumber.countryCode
+            val phone = getEncryptedPhoneNumber(phoneOtp)
             phoneOtp["phone"] = phone
             template.save(phoneOtp, "phone_number_otp")
         }
+    }
+
+    @Changeset(order = 2, id = "encrypted_user_phone_number", author = "Mahmoud")
+    fun userPhoneCryptoChangeSet(template: MongoTemplate) {
+        template.findAll(Document::class.java, "users").forEach { phoneOtp ->
+            val phone = getEncryptedPhoneNumber(phoneOtp)
+            phoneOtp["phone"] = phone
+            template.save(phoneOtp, "users")
+        }
+    }
+
+    private fun getEncryptedPhoneNumber(phoneOtp: Document): Document {
+        val phone = phoneOtp.get("phone", Document::class.java)
+        val phoneNumber = phone.get("phoneNumber", String::class.java)
+        val countryCode = phone.get("countryCode", String::class.java)
+        val encryptedPhoneNumber = phoneNumberCrypto.encrypt(
+            PhoneNumber(
+                phoneNumber,
+                PhoneNumber.CountryCode.entries.first { it.countryCode == countryCode }
+            )
+        )
+        phone["phoneNumber"] = encryptedPhoneNumber.phoneNumber
+        phone["countryCode"] = encryptedPhoneNumber.countryCode
+        return phone
     }
 }
